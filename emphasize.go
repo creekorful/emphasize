b7f57a42e98f966606ae460ee63f7a586e678d2f
Aloïs Micard <alois@micard.lu>
2021-07-13 06:47:31 +0200

    Basic text rendering

--- emphasize.go
+++ emphasize.go
@@ -1,5 +1,16 @@
 package main
 
+import "github.com/fogleman/gg"
+
 func main() {
-   print("Hello, world.")
+	const S = 1024
+	dc := gg.NewContext(S, S)
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+	dc.SetRGB(0, 0, 0)
+	if err := dc.LoadFontFace("/Library/Fonts/Arial Unicode.ttf", 96); err != nil {
+		panic(err)
+	}
+	dc.DrawStringAnchored("Hello, world!", S/2, S/2, 0.5, 0.5)
+	_ = dc.SavePNG("out.png")
 }
