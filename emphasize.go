d2ce8fa06bd64641f9d3c4fbadd204e891c46b4a
Aloïs Micard <alois@micard.lu>
2021-07-13 06:48:38 +0200

    Full HD ftw o/

--- emphasize.go
+++ emphasize.go
@@ -2,15 +2,21 @@ package main
 
 import "github.com/fogleman/gg"
 
+const (
+	width  = 1920
+	height = 1080
+)
+
 func main() {
-	const S = 1024
-	dc := gg.NewContext(S, S)
+	dc := gg.NewContext(width, height)
+
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
+
 	dc.SetRGB(0, 0, 0)
 	if err := dc.LoadFontFace("/Library/Fonts/Arial Unicode.ttf", 96); err != nil {
 		panic(err)
 	}
-	dc.DrawStringAnchored("Hello, world!", S/2, S/2, 0.5, 0.5)
+	dc.DrawStringAnchored("Hello, world!", width/2, height/2, 0.5, 0.5)
 	_ = dc.SavePNG("out.png")
 }
