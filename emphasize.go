e9e65d67c7a23a13ea12edb1cf46fca7987fb38c
Aloïs Micard <alois@micard.lu>
2021-07-13 06:54:10 +0200

    Render my lovely sentence

--- emphasize.go
+++ emphasize.go
@@ -3,20 +3,21 @@ package main
 import "github.com/fogleman/gg"
 
 const (
-	width  = 1920
-	height = 1080
+	width    = 1920
+	height   = 1080
+	sentence = "At least there's stars in the sky."
 )
 
 func main() {
 	dc := gg.NewContext(width, height)
 
-	dc.SetRGB(1, 1, 1)
+	dc.SetRGB255(0, 0, 0)
 	dc.Clear()
 
-	dc.SetRGB(0, 0, 0)
+	dc.SetRGB255(241, 196, 12)
 	if err := dc.LoadFontFace("/Library/Fonts/Arial Unicode.ttf", 96); err != nil {
 		panic(err)
 	}
-	dc.DrawStringAnchored("Hello, world!", width/2, height/2, 0.5, 0.5)
+	dc.DrawStringAnchored(sentence, width/2, height/2, 0.5, 0.5)
 	_ = dc.SavePNG("out.png")
 }
